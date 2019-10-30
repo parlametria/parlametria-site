2b6202b4ba7f4948b09ffea4e671b923dc89160f
Jefferson Neves <jefferson.rpn@gmail.com>
2019-10-30 17:25:02 -0300

    Cria roteamento do app

---
+++ src/app/main/main-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { HomeComponent } from './home/home.component';
+import { SobreComponent } from './sobre/sobre.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'sobre',
+    component: SobreComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class MainRoutingModule { }
