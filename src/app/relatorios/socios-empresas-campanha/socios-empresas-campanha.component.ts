2b9affcdf8e795e7a4fd8917994e6091e1a6f07e
Jefferson Neves <jefferson.rpn@gmail.com>
2019-11-28 17:55:40 -0300

    Insere datatable no relatório de atividades econômicas

--- src/app/relatorios/socios-empresas-campanha/socios-empresas-campanha.component.ts
+++ src/app/relatorios/socios-empresas-campanha/socios-empresas-campanha.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AllCommunityModules } from '@ag-grid-community/all-modules';
+
+import { ApiService } from 'src/app/services/api.service';
+
 @Component({
   selector: 'app-socios-empresas-campanha',
   templateUrl: './socios-empresas-campanha.component.html',
@@ -7,9 +11,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SociosEmpresasCampanhaComponent implements OnInit {
 
-  constructor() { }
+  columnDefs = [
+    { headerName: 'Nome', field: 'nome_eleitoral', sortable: true, filter: 'agTextColumnFilter' },
+    { headerName: 'Atividade Econômica', field: 'grupo_atividade_economica', sortable: true, filter: true },
+    { headerName: 'Proporção da Doação', field: 'proporcao_doacao', sortable: true, filter: true },
+    { headerName: 'Partido', field: 'sg_partido', sortable: true, filter: true },
+    { headerName: 'Total por atividade', field: 'total_por_atividade', sortable: true, filter: 'agNumberColumnFilter' },
+    { headerName: 'Total Recebido', field: 'total_recebido_geral', sortable: true, filter: 'agNumberColumnFilter' },
+    { headerName: 'UF', field: 'uf', sortable: true, filter: true }
+  ];
+
+  rowData: any;
+
+  modules = AllCommunityModules;
+
+  constructor(private apiService: ApiService) { }
 
   ngOnInit() {
+    this.rowData = this.apiService.getJson('deputados-setor-economico.json');
   }
 
 }
