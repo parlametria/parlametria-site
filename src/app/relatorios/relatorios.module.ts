0c67c06d62accec6c80daef2d163eddc4d54dddb
Jefferson Neves <jefferson.rpn@gmail.com>
2019-11-29 12:07:13 -0300

    Move a página de relatórios para o módulo 'relatorios'

--- src/app/relatorios/relatorios.module.ts
+++ src/app/relatorios/relatorios.module.ts
@@ -3,14 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { AgGridModule } from '@ag-grid-community/angular';
 
-import { RelatoriosExibirComponent } from './relatorios-exibir/relatorios-exibir.component';
 import { RelatoriosListarComponent } from './relatorios-listar/relatorios-listar.component';
 import { RelatoriosRoutingModule } from './relatorios-routing.module';
 import { SociosEmpresasCampanhaComponent } from './socios-empresas-campanha/socios-empresas-campanha.component';
 
 @NgModule({
   declarations: [
-    RelatoriosExibirComponent,
     RelatoriosListarComponent,
     SociosEmpresasCampanhaComponent
   ],
